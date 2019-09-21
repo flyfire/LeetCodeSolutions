03058314723d71d5db4c13ea189e8e166f69cce9
Solarex <rh.hou.work@gmail.com>
2019-09-21 16:02:54 +0800

    leetcode

---
+++ src/main/java/com/solarexsoft/leetcode/editor/cn/L20ValidParentheses.java
@@ -0,0 +1,85 @@
+//给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。 
+//
+// 有效字符串需满足： 
+//
+// 
+// 左括号必须用相同类型的右括号闭合。 
+// 左括号必须以正确的顺序闭合。 
+// 
+//
+// 注意空字符串可被认为是有效字符串。 
+//
+// 示例 1: 
+//
+// 输入: "()"
+//输出: true
+// 
+//
+// 示例 2: 
+//
+// 输入: "()[]{}"
+//输出: true
+// 
+//
+// 示例 3: 
+//
+// 输入: "(]"
+//输出: false
+// 
+//
+// 示例 4: 
+//
+// 输入: "([)]"
+//输出: false
+// 
+//
+// 示例 5: 
+//
+// 输入: "{[]}"
+//输出: true 
+// Related Topics 栈 字符串
+/*
+ * Author: Solarex
+ * Solutions: https://github.com/flyfire/LeetCodeSolutions
+ * SolutionComments: https://solarex.github.io/leetcode-solution-comments/
+ */
+package com.solarexsoft.leetcode.editor.cn;
+
+import java.util.Stack;
+
+public class L20ValidParentheses {
+    public static void main(String[] args) {
+         Solution solution = new L20ValidParentheses().new Solution();
+    }
+    
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '{' || c == '[' || c == '(') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                Character topchar = stack.pop();
+                if (c == '}' && topchar != '{') {
+                    return false;
+                }
+                if (c == ']' && topchar != '[') {
+                    return false;
+                }
+                if (c == ')' && topchar != '(') {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
