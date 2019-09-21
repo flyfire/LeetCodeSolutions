eff673d11b03e40186857fb574d6a611909d1fed
Solarex <rh.hou.work@gmail.com>
2019-09-21 20:38:43 +0800

    leetcode

---
+++ src/main/java/com/solarexsoft/leetcode/editor/cn/L203RemoveLinkedListElements.java
@@ -0,0 +1,55 @@
+//删除链表中等于给定值 val 的所有节点。 
+//
+// 示例: 
+//
+// 输入: 1->2->6->3->4->5->6, val = 6
+//输出: 1->2->3->4->5
+// 
+// Related Topics 链表
+/*
+ * Author: Solarex
+ * Solutions: https://github.com/flyfire/LeetCodeSolutions
+ * SolutionComments: https://solarex.github.io/leetcode-solution-comments/
+ */
+package com.solarexsoft.leetcode.editor.cn;
+public class L203RemoveLinkedListElements {
+    public static void main(String[] args) {
+         Solution solution = new L203RemoveLinkedListElements().new Solution();
+    }
+    
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        while (head != null && head.val == val) {
+            ListNode delNode = head;
+            head = delNode.next;
+            delNode.next = null;
+        }
+        if (head == null) {
+            return null;
+        }
+        ListNode prev = head;
+        while (prev.next != null) {
+            if (prev.next.val == val) {
+                ListNode delNode = prev.next;
+                prev.next = delNode.next;
+                delNode.next = null;
+            } else {
+                prev = prev.next;
+            }
+        }
+        return head;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
---
+++ src/main/java/com/solarexsoft/leetcode/editor/cn/ListNode.java
@@ -0,0 +1,11 @@
+package com.solarexsoft.leetcode.editor.cn;
+
+/**
+ * Created by houruhou on 2019/9/21.
+ * Desc:
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
