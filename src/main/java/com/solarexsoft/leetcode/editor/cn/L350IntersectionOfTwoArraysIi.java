8b9f4235e83fa80f813a3f9efce7dee9915beee6
Solarex <rh.hou.work@gmail.com>
2019-10-05 10:30:38 +0800

    map

---
+++ src/main/java/com/solarexsoft/leetcode/editor/cn/L350IntersectionOfTwoArraysIi.java
@@ -0,0 +1,75 @@
+//给定两个数组，编写一个函数来计算它们的交集。 
+//
+// 示例 1: 
+//
+// 输入: nums1 = [1,2,2,1], nums2 = [2,2]
+//输出: [2,2]
+// 
+//
+// 示例 2: 
+//
+// 输入: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+//输出: [4,9] 
+//
+// 说明： 
+//
+// 
+// 输出结果中每个元素出现的次数，应与元素在两个数组中出现的次数一致。 
+// 我们可以不考虑输出结果的顺序。 
+// 
+//
+// 进阶: 
+//
+// 
+// 如果给定的数组已经排好序呢？你将如何优化你的算法？ 
+// 如果 nums1 的大小比 nums2 小很多，哪种方法更优？ 
+// 如果 nums2 的元素存储在磁盘上，磁盘内存是有限的，并且你不能一次加载所有的元素到内存中，你该怎么办？ 
+// 
+// Related Topics 排序 哈希表 双指针 二分查找
+/*
+ * Author: Solarex
+ * Solutions: https://github.com/flyfire/LeetCodeSolutions
+ * SolutionComments: https://solarex.github.io/leetcode-solution-comments/
+ */
+package com.solarexsoft.leetcode.editor.cn;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class L350IntersectionOfTwoArraysIi {
+    public static void main(String[] args) {
+         Solution solution = new L350IntersectionOfTwoArraysIi().new Solution();
+    }
+    
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i : nums1) {
+            if (map.containsKey(i)) {
+                map.put(i, map.get(i) + 1);
+            } else {
+                map.put(i, 1);
+            }
+        }
+        for (int i : nums2) {
+            if (map.containsKey(i)) {
+                list.add(i);
+                map.put(i, map.get(i) - 1);
+                if (map.get(i) == 0) {
+                    map.remove(i);
+                }
+            }
+        }
+        int[] result = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            result[i] = list.get(i);
+        }
+        return result;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
