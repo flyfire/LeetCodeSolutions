67cc2e1ddcab3f6de157f6d3d252d9f14489a0b1
Solarex <rh.hou.work@gmail.com>
2020-01-01 15:36:11 +0800

    reverst linked list

---
+++ src/main/java/com/solarexsoft/leetcode/editor/cn/L206ReverseLinkedList.java
@@ -0,0 +1,47 @@
+//反转一个单链表。 
+//
+// 示例: 
+//
+// 输入: 1->2->3->4->5->NULL
+//输出: 5->4->3->2->1->NULL 
+//
+// 进阶: 
+//你可以迭代或递归地反转链表。你能否用两种方法解决这道题？ 
+// Related Topics 链表
+/*
+ * Author: Solarex
+ * Solutions: https://github.com/flyfire/LeetCodeSolutions
+ * SolutionComments: https://solarex.github.io/leetcode-solution-comments/
+ */
+package com.solarexsoft.leetcode.editor.cn;
+public class L206ReverseLinkedList {
+    public static void main(String[] args) {
+         Solution solution = new L206ReverseLinkedList().new Solution();
+    }
+    
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode cur = head;
+        while (cur != null) {
+            ListNode tmp = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = tmp;
+        }
+        return prev;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
