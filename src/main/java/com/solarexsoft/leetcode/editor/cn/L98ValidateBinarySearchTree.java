c554cd5c7a521e7822f9d17dde50fb926315861c
Solarex <rh.hou.work@gmail.com>
2020-01-02 18:08:43 +0800

    validate bst

---
+++ src/main/java/com/solarexsoft/leetcode/editor/cn/L98ValidateBinarySearchTree.java
@@ -0,0 +1,69 @@
+//给定一个二叉树，判断其是否是一个有效的二叉搜索树。 
+//
+// 假设一个二叉搜索树具有如下特征： 
+//
+// 
+// 节点的左子树只包含小于当前节点的数。 
+// 节点的右子树只包含大于当前节点的数。 
+// 所有左子树和右子树自身必须也是二叉搜索树。 
+// 
+//
+// 示例 1: 
+//
+// 输入:
+//    2
+//   / \
+//  1   3
+//输出: true
+// 
+//
+// 示例 2: 
+//
+// 输入:
+//    5
+//   / \
+//  1   4
+//     / \
+//    3   6
+//输出: false
+//解释: 输入为: [5,1,4,null,null,3,6]。
+//     根节点的值为 5 ，但是其右子节点值为 4 。
+// 
+// Related Topics 树 深度优先搜索
+/*
+ * Author: Solarex
+ * Solutions: https://github.com/flyfire/LeetCodeSolutions
+ * SolutionComments: https://solarex.github.io/leetcode-solution-comments/
+ */
+package com.solarexsoft.leetcode.editor.cn;
+public class L98ValidateBinarySearchTree {
+    public static void main(String[] args) {
+         Solution solution = new L98ValidateBinarySearchTree().new Solution();
+    }
+    
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return isValidBSTRecursive(root, null, null);
+    }
+
+    public boolean isValidBSTRecursive(TreeNode root, TreeNode lower, TreeNode upper) {
+        if (root == null) return true;
+        if (lower != null && lower.val >= root.val) return false;
+        if (upper != null && upper.val <= root.val) return false;
+        return isValidBSTRecursive(root.left, lower, root) && isValidBSTRecursive(root.right, root, upper);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
---
+++ src/main/java/com/solarexsoft/leetcode/editor/cn/TreeNode.java
@@ -0,0 +1,14 @@
+package com.solarexsoft.leetcode.editor.cn;
+
+/**
+ * Created by houruhou on 2020/1/2.
+ * Desc:
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) {
+        val = x;
+    }
+}
