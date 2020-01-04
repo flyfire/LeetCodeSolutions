001a95432c666b77a34ac76b2c4cd50e29d10470
Solarex <rh.hou.work@gmail.com>
2020-01-04 15:52:50 +0800

    min tree depth

--- src/main/java/com/solarexsoft/leetcode/editor/cn/L111MinimumDepthOfBinaryTree.java
+++ src/main/java/com/solarexsoft/leetcode/editor/cn/L111MinimumDepthOfBinaryTree.java
@@ -41,6 +41,7 @@ public class L111MinimumDepthOfBinaryTree {
  */
 class Solution {
     public int minDepth(TreeNode root) {
+        /*
         if (root == null) {
             return 0;
         }
@@ -54,6 +55,14 @@ class Solution {
         int rightMin = minDepth(root.right);
 
         return 1 + Math.min(leftMin, rightMin);
+        */
+        if (root == null) {
+            return 0;
+        }
+        int left = minDepth(root.left);
+        int right = minDepth(root.right);
+
+        return (left == 0 || right == 0) ? left + right + 1 : Math.min(left,right) + 1;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
