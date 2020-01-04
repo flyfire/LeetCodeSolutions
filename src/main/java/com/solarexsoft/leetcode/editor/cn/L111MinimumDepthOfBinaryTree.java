ec394cf4043b0f655eccafc572429f86f0695bf1
Solarex <rh.hou.work@gmail.com>
2020-01-04 15:49:09 +0800

    min tree depth

---
+++ src/main/java/com/solarexsoft/leetcode/editor/cn/L111MinimumDepthOfBinaryTree.java
@@ -0,0 +1,61 @@
+//给定一个二叉树，找出其最小深度。 
+//
+// 最小深度是从根节点到最近叶子节点的最短路径上的节点数量。 
+//
+// 说明: 叶子节点是指没有子节点的节点。 
+//
+// 示例: 
+//
+// 给定二叉树 [3,9,20,null,null,15,7], 
+//
+//     3
+//   / \
+//  9  20
+//    /  \
+//   15   7 
+//
+// 返回它的最小深度 2. 
+// Related Topics 树 深度优先搜索 广度优先搜索
+
+/*
+ * Author: Solarex
+ * Solutions: https://github.com/flyfire/LeetCodeSolutions
+ * SolutionComments: https://solarex.github.io/leetcode-solution-comments/
+ */
+package com.solarexsoft.leetcode.editor.cn;
+public class L111MinimumDepthOfBinaryTree {
+    public static void main(String[] args) {
+         Solution solution = new L111MinimumDepthOfBinaryTree().new Solution();
+    }
+    
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.left == null) {
+            return 1 + minDepth(root.right);
+        }
+        if (root.right == null) {
+            return 1 + minDepth(root.left);
+        }
+        int leftMin = minDepth(root.left);
+        int rightMin = minDepth(root.right);
+
+        return 1 + Math.min(leftMin, rightMin);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
