c3003718886928987cc987f134cde26698ca5692
Solarex <rh.hou.work@gmail.com>
2020-01-07 19:59:30 +0800

    max

---
+++ src/main/java/com/solarexsoft/leetcode/editor/cn/L152MaximumProductSubarray.java
@@ -0,0 +1,51 @@
+//给定一个整数数组 nums ，找出一个序列中乘积最大的连续子序列（该序列至少包含一个数）。 
+//
+// 示例 1: 
+//
+// 输入: [2,3,-2,4]
+//输出: 6
+//解释: 子数组 [2,3] 有最大乘积 6。
+// 
+//
+// 示例 2: 
+//
+// 输入: [-2,0,-1]
+//输出: 0
+//解释: 结果不能为 2, 因为 [-2,-1] 不是子数组。 
+// Related Topics 数组 动态规划
+
+/*
+ * Author: Solarex
+ * Solutions: https://github.com/flyfire/LeetCodeSolutions
+ * SolutionComments: https://solarex.github.io/leetcode-solution-comments/
+ */
+package com.solarexsoft.leetcode.editor.cn;
+public class L152MaximumProductSubarray {
+    public static void main(String[] args) {
+         Solution solution = new L152MaximumProductSubarray().new Solution();
+    }
+    
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int maxProduct(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int max = Integer.MIN_VALUE, imax = 1, imin = 1;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] < 0) {
+                int tmp = imax;
+                imax = imin;
+                imin = tmp;
+            }
+            imax = Math.max(imax * nums[i], nums[i]);
+            imin = Math.min(imin * nums[i], nums[i]);
+            max = Math.max(imax, max);
+        }
+        return max;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
