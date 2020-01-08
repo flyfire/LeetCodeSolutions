75233a26deaa2e0421052b4cfaf57c2bb2eff3f4
Solarex <rh.hou.work@gmail.com>
2020-01-08 21:31:49 +0800

    single number

---
+++ src/main/java/com/solarexsoft/leetcode/editor/cn/L137SingleNumberIi.java
@@ -0,0 +1,59 @@
+//给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现了三次。找出那个只出现了一次的元素。 
+//
+// 说明： 
+//
+// 你的算法应该具有线性时间复杂度。 你可以不使用额外空间来实现吗？ 
+//
+// 示例 1: 
+//
+// 输入: [2,2,3,2]
+//输出: 3
+// 
+//
+// 示例 2: 
+//
+// 输入: [0,1,0,1,0,1,99]
+//输出: 99 
+// Related Topics 位运算
+
+/*
+ * Author: Solarex
+ * Solutions: https://github.com/flyfire/LeetCodeSolutions
+ * SolutionComments: https://solarex.github.io/leetcode-solution-comments/
+ */
+package com.solarexsoft.leetcode.editor.cn;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class L137SingleNumberIi {
+    public static void main(String[] args) {
+         Solution solution = new L137SingleNumberIi().new Solution();
+    }
+    
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int singleNumber(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                map.put(nums[i], map.get(nums[i]) + 1);
+            } else {
+                map.put(nums[i], 1);
+            }
+        }
+        Iterator<Map.Entry<Integer, Integer>> iterator = map.entrySet().iterator();
+        while (iterator.hasNext()) {
+            Map.Entry<Integer, Integer> next = iterator.next();
+            if (next.getValue() == 1) {
+                return next.getKey();
+            }
+        }
+        return -1;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
