12686e52dbcc87dcee2dd0c63d253b2fa94cf8f8
Solarex <rh.hou.work@gmail.com>
2020-01-09 17:38:26 +0800

    binary search

---
+++ src/main/java/com/solarexsoft/leetcode/editor/cn/L154FindMinimumInRotatedSortedArrayIi.java
@@ -0,0 +1,58 @@
+//假设按照升序排序的数组在预先未知的某个点上进行了旋转。 
+//
+// ( 例如，数组 [0,1,2,4,5,6,7] 可能变为 [4,5,6,7,0,1,2] )。 
+//
+// 请找出其中最小的元素。 
+//
+// 注意数组中可能存在重复的元素。 
+//
+// 示例 1： 
+//
+// 输入: [1,3,5]
+//输出: 1 
+//
+// 示例 2： 
+//
+// 输入: [2,2,2,0,1]
+//输出: 0 
+//
+// 说明： 
+//
+// 
+// 这道题是 寻找旋转排序数组中的最小值 的延伸题目。 
+// 允许重复会影响算法的时间复杂度吗？会如何影响，为什么？ 
+// 
+// Related Topics 数组 二分查找
+
+/*
+ * Author: Solarex
+ * Solutions: https://github.com/flyfire/LeetCodeSolutions
+ * SolutionComments: https://solarex.github.io/leetcode-solution-comments/
+ */
+package com.solarexsoft.leetcode.editor.cn;
+public class L154FindMinimumInRotatedSortedArrayIi {
+    public static void main(String[] args) {
+         Solution solution = new L154FindMinimumInRotatedSortedArrayIi().new Solution();
+    }
+    
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int findMin(int[] nums) {
+        int low = 0, high = nums.length - 1;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] > nums[high]) {
+                low = mid + 1;
+            } else if (nums[mid] < nums[high]) {
+                high = mid;
+            } else {
+                high--;
+            }
+        }
+        return nums[low];
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
